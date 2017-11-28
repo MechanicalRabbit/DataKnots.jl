cfab5cc70560dab960d0a3b453d9c5a257dce6d0
autodocs <autodocs>
2017-11-28 03:49:24 +0000

    build based on 5aac98a

---
+++ latest/search_index.js
@@ -0,0 +1,67 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "QueryCombinators.jl Documentation",
+    "title": "QueryCombinators.jl Documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#QueryCombinators.jl-Documentation-1",
+    "page": "QueryCombinators.jl Documentation",
+    "title": "QueryCombinators.jl Documentation",
+    "category": "section",
+    "text": "QueryCombinators is a Julia library that implements a combinator-based embedded query language."
+},
+
+{
+    "location": "index.html#Contents-1",
+    "page": "QueryCombinators.jl Documentation",
+    "title": "Contents",
+    "category": "section",
+    "text": "Pages = [\n    \"reference.md\",\n    \"test/index.md\",\n]"
+},
+
+{
+    "location": "index.html#Index-1",
+    "page": "QueryCombinators.jl Documentation",
+    "title": "Index",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "reference.html#",
+    "page": "API Reference",
+    "title": "API Reference",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "reference.html#API-Reference-1",
+    "page": "API Reference",
+    "title": "API Reference",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "test/index.html#",
+    "page": "Test Suite",
+    "title": "Test Suite",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "test/index.html#Test-Suite-1",
+    "page": "Test Suite",
+    "title": "Test Suite",
+    "category": "section",
+    "text": "Pages = [\n]"
+},
+
+]}
