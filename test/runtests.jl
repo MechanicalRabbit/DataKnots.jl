160ad2ac2f57db1367e2f84201315d09547c391c
Kirill Simonov <xi@resolvent.net>
2018-11-18 16:57:29 -0600

    clone NarrativeTest since it is not yet registered

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env julia
 
 using Pkg
-haskey(Pkg.installed(), "NarrativeTest") || Pkg.add("NarrativeTest")
+haskey(Pkg.installed(), "NarrativeTest") || Pkg.clone("https://github.com/rbt-lang/NarrativeTest.jl")
 
 # Make `print(Int)` produce identical output on 32-bit and 64-bit platforms.
 Base.show_datatype(io::IO, ::Type{Int}) = print(io, "Int")
