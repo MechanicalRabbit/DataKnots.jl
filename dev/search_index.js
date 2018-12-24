1af15f9f2662c9c34c71ebcd26f69aec73e97fa9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-24 14:47:03 +0000

    build based on 43fd584

--- dev/search_index.js
+++ dev/search_index.js
@@ -914,18 +914,26 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "generated/simulation/#",
-    "page": "Example: Simulated Data",
-    "title": "Example: Simulated Data",
+    "page": "Tutorial: Simulated Data",
+    "title": "Tutorial: Simulated Data",
     "category": "page",
     "text": "EditURL = \"https://github.com/rbt-lang/DataKnots.jl/blob/master/doc/src/simulation.jl\""
 },
 
 {
-    "location": "generated/simulation/#Example:-Simulated-Data-1",
-    "page": "Example: Simulated Data",
-    "title": "Example: Simulated Data",
+    "location": "generated/simulation/#Tutorial:-Simulated-Data-1",
+    "page": "Tutorial: Simulated Data",
+    "title": "Tutorial: Simulated Data",
     "category": "section",
-    "text": "DataKnots can be used to create simulated data sets, using simple range iteration and random number generation.using DataKnotsLet us plot something; we\'ll change this soon.using Plots\nx = range(0, stop=6pi, length=1000)\nplot(x, [sin.(x), cos.(x)])"
+    "text": "In this tutorial we simulate a random patient population from a health clinic dealing with hypertension and type 2 diabetes. This tutorial assumes the reader has read \"Thinking in Combinators\".Let\'s start with some preliminary imports.using JSON, UUIDs, Plots, Dates\nusing DataKnots\nusing Distributions\nusing Random: seed!, rand\n\nseed!(42); # hide"
+},
+
+{
+    "location": "generated/simulation/#Lifting-Random-Generators-1",
+    "page": "Tutorial: Simulated Data",
+    "title": "Lifting Random Generators",
+    "category": "section",
+    "text": "For this simulation, we would like a random number of patients. To get a random integer from the base library, use the rand function with a range input, rand(3:5). To perform a similar function in DataKnots, we follow three steps. First, lift the rand function into a combinator, Rand. Second, this combinator can be invoked to produce a pipeline, R3to5. Third, the pipeline is run to produce a dataknot having the random value.Rand(r::AbstractVector) = Combinator(it -> rand(r))(It)\nR3to5 = Rand(3:5)\nrun(R3to5)In this definition, the Rand combinator creates pipelines that ignore their implicit input, It. There are more interesting combinators that take their pipeline input into consideration. For simple cases, this indirection is burdensome, however, we bring the idea of being random into our combinator algebra where it can be combined with other concepts. For example, we could now produce output with a random number of entries (similar to 1:rand(3:5)).run(Range(Rand(3:5)))For each of our simulated patients, we need to assign them a biological sex.@enum Sex male=1 female=2\nrand_sex() = Sex(rand(Categorical([.492, .508])))\nRand(::Type{Sex}) = :sex => Combinator(rand_sex)()\n\nrun(Range(Rand(3:5)) >> Rand(Sex))"
 },
 
 ]}
