fe7fc3311e88f7f7957d1ef029b725b3fafdb27e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-24 20:41:56 +0000

    build based on 5d5efc7

--- dev/simulation.jl
+++ dev/simulation.jl
@@ -12,39 +12,46 @@ using DataKnots
 using Distributions
 using Random: seed!, rand
 
-seed!(42); # hide
+seed!(0); # hide
 
-# ## Lifting Random Generators
+# ## Lifting Generators
 #
 # For this simulation, we would like a random number of
-# patients. To get a random integer from the base library,
-# use the `rand` function with a `range` input, `rand(3:5)`.
-# To perform a similar function in DataKnots, we follow three
-# steps. First, lift the `rand` function into a *combinator*,
-# `Rand`. Second, this combinator can be invoked to produce
-# a *pipeline*, `R3to5`. Third, the pipeline is `run` to
-# produce a dataknot having the random value.
+# patients. This can be done using the `Range` combinator.
+# In this example, the argument, `rand(3:5)` is immediately
+# evaluated, and converted into a constant `Pipeline`.
 
-Rand(r::AbstractVector) = Combinator(it -> rand(r))(It)
-R3to5 = Rand(3:5)
-run(R3to5)
+run(Range(rand(3:5)))
 
-# In this definition, the `Rand` combinator creates pipelines
-# that ignore their implicit input, `It`. There are more
-# interesting combinators that take their pipeline input into
-# consideration. For simple cases, this indirection is
-# burdensome, however, we bring the idea of being random into
-# our combinator algebra where it can be combined with other
-# concepts. For example, we could now produce output with a
-# random number of entries (similar to `1:rand(3:5)`).
+# If we want a different random number for each pipeline
+# input, we need to lift the `rand` function to a combinator.
 
-run(Range(Rand(3:5)))
+Rand(r::AbstractVector) = Combinator(() -> rand(r))()
+run(Range(3) >> Rand(1:9))
 
-# For each of our simulated patients, we need to assign
-# them a biological sex.
+# Suppose each patient is assigned a random 5-digit Medical
+# Record Number ("MRN"). This concept could be defined and
+# independently tested.
+
+RandMRN() =
+  :mrn => Rand(10000:99999)
+run(RandMRN())
+
+# For each of our simulated patients, we need to assign them
+# a biological sex as well. In pure Julia, this could be done
+# with a `Sex` and a `rand_sex` function.
 
 @enum Sex male=1 female=2
 rand_sex() = Sex(rand(Categorical([.492, .508])))
-Rand(::Type{Sex}) = :sex => Combinator(rand_sex)()
+RandSex() =
+  :sex => Combinator(rand_sex)()
+run(RandSex())
 
-run(Range(Rand(3:5)) >> Rand(Sex))
+# With these primitives, we could start building our sample
+# data set. What's important already is that the definition
+# of Patient can be seen to be independent of its components.
+
+RandPatient() =
+  :patient => Record(RandMRN(), RandSex())
+run(Range(Rand(3:5)) >> RandPatient())
+
