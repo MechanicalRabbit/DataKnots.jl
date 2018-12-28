98e3f4fecde46d9b08019513006f22230abd67c2
Clark Evans <cce@clarkevans.com>
2018-12-28 14:00:41 -0500

    simplify Rand lift

--- doc/src/simulation.jl
+++ doc/src/simulation.jl
@@ -1,9 +1,9 @@
 # # Tutorial: Simulated Data
 #
 # In this tutorial we simulate a random patient population from a
-# health clinic dealing with hypertension and type 2 diabetes. This
-# tutorial assumes the reader has read "Thinking in Combinators" and
-# wishes to use `DataKnots`.
+# health clinic dealing with hypertension and type 2 diabetes. We
+# assume the reader has read "Thinking in Combinators" and wishes to
+# use `DataKnots` for this simulation.
 
 using DataKnots
 
@@ -16,14 +16,13 @@ using DataKnots
 OneTo(N) = Lift(UnitRange, (1, N))
 run(:patient => OneTo(3))
 
-# Known data is boring in a simulation. What's interesting is
-# pseudorandom data. To make that data repeatable, we need to fix the
-# `seed`. We can then lift the `rand` function to a combinator that
-# takes anything that's a vector.
+# Known data is boring in a simulation. Instead we need pseudorandom
+# data. To make that data repeatable, let's fix the `seed`. We can then
+# lift the `rand` function to a DataKnot combinator.
 
 using Random: seed!, rand
 seed!(1)
-Rand(r::AbstractVector) = Lift(rand, (r,));
+Rand(r) = Lift(rand, (r,));
 
 # Suppose each patient is assigned a random 5-digit Medical Record
 # Number ("MRN"). Let's define and test this concept.
@@ -32,14 +31,11 @@ RandMRN =
   :mrn => Rand(10000:99999)
 run(RandMRN)
 
-# Sometimes it's useful to use categorical distributions. We can also
-# use distributions within our `Rand` combinator. Let's suppose that
-# 49.2% of patients are male and 50.8% are female. Hence, we could
-# randomly choose 1 = Male, or 2 = Female as follows.
+# To assign Sometimes it's useful to use categorical distributions.
+# Let's define a random male/female sex distribution.
 
 using Distributions
 SexDist = Categorical([.492, .508])
-Rand(d::Distribution) = Lift(rand, (d,))
 run(Rand(SexDist))
 
 # While this is nice, it makes it challenging to remember which is a
@@ -51,11 +47,15 @@ RandSex =
   :sex => Lift(Sex, (Rand(SexDist),))
 run(RandSex)
 
-# With these primitives, we could start building our sample
-# data set. What's important already is that the definition
-# of Patient can be seen to be independent of its components.
+# With these primitives, we could start building our sample data set.
+# Notice how each combinator can be defined/tested independently and
+# then arranged to build a set of random patients.
 
 RandPatient =
   :patient => Record(RandMRN, RandSex)
 run(OneTo(Rand(3:5)) >> RandPatient)
+
+#
+# ## Function Composition via Combinators
+#
 
