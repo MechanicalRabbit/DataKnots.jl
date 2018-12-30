b8878b153ba32563c7da15281e216f6154c0140c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-30 13:42:43 +0000

    build based on 1d181cf

--- dev/simulation.jl
+++ dev/simulation.jl
@@ -18,11 +18,13 @@ run(:patient => OneTo(3))
 
 # Known data is boring in a simulation. Instead we need pseudorandom
 # data. To make that data repeatable, let's fix the `seed`. We can then
-# lift the `rand` function to a DataKnot combinator.
+# lift the `rand` function to a DataKnot combinator and use it to pick
+# a random number from 3 to 5.
 
 using Random: seed!, rand
 seed!(1)
-Rand(r) = Lift(rand, (r,));
+Rand(r::AbstractVector) = Lift(rand, (r,))
+run(Rand(3:5))
 
 # Suppose each patient is assigned a random 5-digit Medical Record
 # Number ("MRN"). Let's define and test this concept.
@@ -31,16 +33,17 @@ RandMRN =
   :mrn => Rand(10000:99999)
 run(RandMRN)
 
-# To assign Sometimes it's useful to use categorical distributions.
-# Let's define a random male/female sex distribution.
+# To assign a sex to each patient, we will use categorical
+# distributions. Let's define a random male/female sex distribution.
 
 using Distributions
 SexDist = Categorical([.492, .508])
+Rand(d::Distribution) = Lift(rand, (d,))
 run(Rand(SexDist))
 
-# While this is nice, it makes it challenging to remember which is a
-# male or a female. Julia has an enumerated type for this purpose and
-# we can lift this as well.
+# While this picks `1` or `2` for us, remembering which is male and
+# which is female can be a challenge.  Julia has an enumerated type for
+# this purpose and we can lift this as well.
 
 @enum Sex male=1 female=2
 RandSex =
@@ -48,8 +51,8 @@ RandSex =
 run(RandSex)
 
 # With these primitives, we could start building our sample data set.
-# Notice how each combinator can be defined/tested independently and
-# then arranged to build a set of random patients.
+# Notice how `RandMRN` and `RandSex` are independently designed/tested
+# and then arranged subsequently to build a set of random patients.
 
 RandPatient =
   :patient => Record(RandMRN, RandSex)
