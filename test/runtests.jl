1dcbc27588d13da422c4a226d76d9a893aa0a487
Kirill Simonov <xi@resolvent.net>
2019-02-19 22:13:59 -0600

    fixed tests under julia 1.2-DEV

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,27 @@ Base.show_datatype(io::IO, ::Type{UInt}) = print(io, "UInt")
 # Normalize printing of `Vector{Bool}`.
 Base.show(io::IO, b::Bool) = print(io, get(io, :typeinfo, Any) === Bool ? (b ? "1" : "0") : (b ? "true" : "false"))
 
+# Normalize `LoadError` output and `[0 => 0]` under 1.2.
+if VERSION >= v"1.2.0-DEV"
+    function Base.showerror(io::IO, ex::LoadError, bt; backtrace=true)
+        print(io, "LoadError: ")
+        showerror(io, ex.error, bt, backtrace=backtrace)
+        print(io, "\nin expression starting at $(ex.file):$(ex.line)")
+    end
+    function Base.show(io::IO, p::Pair)
+        iocompact = IOContext(io, :compact => get(io, :compact, true))
+        Base.isdelimited(io, p) && return Base.show_default(iocompact, p)
+        typeinfos = Base.gettypeinfos(io, p)
+        for i = (1, 2)
+            io_i = IOContext(iocompact, :typeinfo => typeinfos[i])
+            Base.isdelimited(io_i, p[i]) || print(io, "(")
+            show(io_i, p[i])
+            Base.isdelimited(io_i, p[i]) || print(io, ")")
+            i == 1 && print(io, get(io, :compact, true) ? "=>" : " => ")
+        end
+    end
+end
+
 using DataKnots
 using NarrativeTest
 
