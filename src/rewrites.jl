fad5c2e258279fbfc4e44ec136e9bc4f57d749c8
Clark C. Evans <cce@clarkevans.com>
2020-09-05 11:40:12 -0500

    implement delinearize to increase readability of query post optimization

--- src/rewrites.jl
+++ src/rewrites.jl
@@ -446,3 +446,42 @@ function linearize(p::Pipeline)::Vector{Pipeline}
     return retval
 end
 
+function delinearize!(vp::Vector{Pipeline})::Pipeline
+    chain = Vector{Pipeline}()
+    while length(vp) > 0
+        @match_pipeline if (vp[1] ~ with_elements(arg))
+            push!(chain, delinearize_elements!(vp))
+        elseif (vp[1] ~ tuple_of(lbls, width::Int))
+            popfirst!(vp)
+            push!(chain, delinearize_columns!(vp, lbls, width))
+        else
+            push!(chain, popfirst!(vp))
+        end
+    end
+    return chain_of(chain...)
+end
+
+function delinearize_elements!(vp::Vector{Pipeline})::Pipeline
+    chain = Vector{Pipeline}()
+    while length(vp) > 0 && vp[1].op == with_elements
+        push!(chain, popfirst!(vp).args[1])
+    end
+    return with_elements(delinearize!(chain))
+end
+
+function delinearize_columns!(vp::Vector{Pipeline}, lbls, width)::Pipeline
+    slots = [Pipeline[] for x in 1:width]
+    while length(vp) > 0 && vp[1].op == with_column
+        idx = vp[1].args[1]
+        if !isa(idx, Int)
+            idx = findfirst(==(idx), lbls)
+            if isnothing(idx)
+                # found a column that depends upon the shape of the
+                # input; stop delinearizing
+                break
+            end
+        end
+        push!(slots[idx], popfirst!(vp).args[2])
+    end
+    return tuple_of(lbls, [delinearize!(cv) for cv in slots])
+end
